1c4c29525a32ace3ea79a3516a3d756b74ef01c6
itzhimangshu <itzhimangshu@gmail.com>
2024-12-22 17:01:34 +0530

    added css properties for p tag

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <h1 className="text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-700 leading-tight">
           Remove the <br /> <span className="bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent">background</span> from <br /> images for free.
         </h1>
-        <p>
+        <p className="my-6 text-[15px] text-gray-500">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi sed
           odit possimus <br /> sequi in repellat dignissimos sunt optio.
           Architecto iste neque at.
