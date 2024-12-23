d84c891e5371a93b9e0038b28299cba40ccea0f2
itzhimangshu <itzhimangshu@gmail.com>
2024-12-23 14:51:38 +0530

    building the steps component

---
+++ client/src/components/Steps.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Steps = () => {
+  return (
+    <div>
+      <h1>Steps to remove background <br /> image in seconds</h1>
+      <div>
+
+        <div>
+            <img src={assets.upload_icon} alt="" />
+            <div>
+                <p>Upload image</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nam soluta distinctio eveniet.</p>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Steps
