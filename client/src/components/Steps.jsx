8c70f8a27f87b41f427e8447d00bb05cc4d1d85f
itzhimangshu <itzhimangshu@gmail.com>
2024-12-23 15:27:13 +0530

    added css to the boxes in steps component

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -4,14 +4,30 @@ import { assets } from '../assets/assets'
 const Steps = () => {
   return (
     <div className='mx-4 lg:mx-44 py-20 xl:py-40'>
-      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>Steps to remove background <br /> image in seconds</h1>
+      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent py-1'>Steps to remove background <br /> image in seconds</h1>
       <div className='flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center'>
 
-        <div>
-            <img src={assets.upload_icon} alt="" />
+        <div className='flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500'>
+            <img className='max-w-9' src={assets.upload_icon} alt="" />
             <div>
-                <p>Upload image</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nam soluta distinctio eveniet.</p>
+                <p className='text-xl font-medium'>Upload image</p>
+                <p className='text-sm text-neutral-500 mt-1'>Lorem ipsum dolor sit amet elit. <br /> Facilis nam soluta distinctio eveniet.</p>
+            </div>
+        </div>
+
+        <div className='flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500'>
+            <img className='max-w-9' src={assets.remove_bg_icon} alt="" />
+            <div>
+                <p className='text-xl font-medium'>Remove background</p>
+                <p className='text-sm text-neutral-500 mt-1'>Lorem ipsum dolor sit amet elit. <br /> Facilis nam soluta distinctio eveniet.</p>
+            </div>
+        </div>
+
+        <div className='flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500'>
+            <img className='max-w-9' src={assets.download_icon} alt="" />
+            <div>
+                <p className='text-xl font-medium'>Download image</p>
+                <p className='text-sm text-neutral-500 mt-1'>Lorem ipsum dolor sit amet elit. <br /> Facilis nam soluta distinctio eveniet.</p>
             </div>
         </div>
 
