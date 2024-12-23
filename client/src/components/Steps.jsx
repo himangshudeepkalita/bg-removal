787935c08d252309dc3d973b7607407ef90bcc0c
itzhimangshu <itzhimangshu@gmail.com>
2024-12-23 15:05:41 +0530

    added some css properties in steps component

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -3,9 +3,9 @@ import { assets } from '../assets/assets'
 
 const Steps = () => {
   return (
-    <div>
-      <h1>Steps to remove background <br /> image in seconds</h1>
-      <div>
+    <div className='mx-4 lg:mx-44 py-20 xl:py-40'>
+      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>Steps to remove background <br /> image in seconds</h1>
+      <div className='flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center'>
 
         <div>
             <img src={assets.upload_icon} alt="" />
