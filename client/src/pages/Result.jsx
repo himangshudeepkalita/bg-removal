efc53596bce7c61b84e3589abd6fa75081ba8670
itzhimangshu <itzhimangshu@gmail.com>
2024-12-29 13:24:11 +0530

    building result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Result = () => {
   return (
-    <div>
-      Result
+    <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
+      
+      <div>
+
+        {/* Image Container */}
+        <div>
+          {/* Left Side */}
+          <div>
+            <p>Original</p>
+            <img src={assets.image_w_bg} alt="" />
+          </div>
+
+          {/* Right Side */}
+          <div>
+            <p>Background Removed</p>
+            <div>
+              <img src={assets.image_wo_bg} alt="" />
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
