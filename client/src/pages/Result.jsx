0d1f6c56adab1e02e3a36092ac768546fe09b066
itzhimangshu <itzhimangshu@gmail.com>
2024-12-29 13:36:31 +0530

    added css in result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -5,20 +5,20 @@ const Result = () => {
   return (
     <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
       
-      <div>
+      <div className='bg-white rounded-lg px-8 py-6 drop-shadow-sm'>
 
         {/* Image Container */}
-        <div>
+        <div className='flex flex-col sm:grid grid-cols-2 gap-8'>
           {/* Left Side */}
           <div>
-            <p>Original</p>
-            <img src={assets.image_w_bg} alt="" />
+            <p className='font-semibold text-gray-600 mb-2'>Original</p>
+            <img className='rounded-md border' src={assets.image_w_bg} alt="" />
           </div>
 
           {/* Right Side */}
-          <div>
-            <p>Background Removed</p>
-            <div>
+          <div className='flex flex-col'>
+            <p className='font-semibold text-gray-600 mb-2'>Background Removed</p>
+            <div className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
               <img src={assets.image_wo_bg} alt="" />
             </div>
           </div>
